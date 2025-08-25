7336d490c21a90af882c720e808acbba0b98a09c
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-08-25 14:38:08 -0300

    feat: added Cliente.java in domain

---
+++ src/domain/Cliente.java
@@ -0,0 +1,105 @@
+/**
+ * @author jawc
+ */
+package domain;
+
+import java.util.Objects;
+
+public class Cliente {
+
+    private String nome;
+    private Long cpf;
+    private Long tel;
+    private String end;
+    private Integer num;
+    private String cidade;
+    private String estado;
+    private String email;
+
+    public Cliente(String nome, Long cpf, Long tel, String end, Integer num, String cidade, String estado, String email) {
+        this.nome = nome;
+        this.cpf = Long.valueOf(cpf);
+        this.tel = Long.valueOf(tel);
+        this.end = end;
+        this.num = Integer.valueOf(num);
+        this.cidade = cidade;
+        this.estado = estado;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+
+    public Long getTel() {
+        return tel;
+    }
+
+    public void setTel(Long tel) {
+        this.tel = tel;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(cpf, cliente.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(cpf);
+    }
+}
