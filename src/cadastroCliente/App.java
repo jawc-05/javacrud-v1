edcddc3fab7de61271870a86748d049010909437
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-08-26 14:47:20 -0300

    feat: added App.java

---
+++ src/cadastroCliente/App.java
@@ -0,0 +1,17 @@
+package cadastroCliente;
+
+/**
+ * 
+ * @author jawc-05
+ * 
+ * /
+
+
+public class App {
+    
+    public static void main(String[] arg){
+        MainScreen screen = new MainScreen();
+        screen.setVisible(true);
+    }
+    
+}
